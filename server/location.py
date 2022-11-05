1a3d70a309530f533d11d3e860e777ff2fb939b8
aayzhao <aaron.y.zhao1@gmail.com>
2022-11-05 14:27:24 -0400

    fucntion

--- server/location.py
+++ server/location.py
@@ -3,6 +3,8 @@ import requests
 from client import Location
 import env
 import bs4
+import math
+
 def get_location(ip):
     # Returns: (Latitude: str, Longitude: str)
     # Raises: RuntimeError
@@ -24,9 +26,31 @@ def get_location(ip):
 
 
 def distance(loc_1: Location, loc_2: Location) -> float:
-    # calculate the distance between 2 points
-    return 0
+
+
+    # calculate the distance between 2 points and returns a distance in km.
+    radius = 6371  # km
+
+    dlat = math.radians(loc_2.latitude - loc_1.latitude)
+    dlon = math.radians(loc_2.longitude - loc_1.longitude)
+    a = (math.sin(dlat / 2) * math.sin(dlat / 2) +
+         math.cos(math.radians(loc_1.latitude)) * math.cos(math.radians(loc_2.latitude)) *
+         math.sin(dlon / 2) * math.sin(dlon / 2))
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    d = radius * c
+
+    return d # distance in km
 
 def intersect(distance, time_1, time_2, speed_1, speed_2) -> bool:
     
     return False
+
+
+if __name__ == "__main__":
+    loc_1 = Location(10.1824542, -25.1389054)
+    
+    loc_2 = Location(45.0123102, -231.120943)
+
+    print(distance(loc_1, loc_2))
+
+    
