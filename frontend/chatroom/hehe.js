3a393294c035eab38ab19c0be916d10f36492a56
Chickenislife6 <tylerliuyang@gmail.com>
2022-11-06 08:26:22 -0500

    eh

--- frontend/chatroom/hehe.js
+++ frontend/chatroom/hehe.js
@@ -1,30 +1,21 @@
 let append = localStorage.getItem("storedChatRoom") === null ? "" : localStorage.getItem("storedChatRoom");
 let mySocket = new WebSocket("ws://127.0.0.1:8080/"+append);
-
+let username = localStorage.getItem("storedUsername");
 mySocket.onmessage = function (event) {
     console.log(event.data)
-    let newElement = document.createElement("p")
-    newElement.classList.add("that");
-    newElement.innerHTML = event.data;
-    document.getElementById('chat').appendChild(newElement)
+    add_message(event.data, className="that")
 }
 
 
 window.addEventListener('load', main);
 function main(){
-    //alert(localStorage.getItem("storedUsername"));
-    console.log(document.getElementById("submitBtn").className)
     document.getElementById("submitBtn").addEventListener("click", (event) => {
         event.preventDefault();
         let txt = document.getElementById("msg");
         outgoing_msg = txt.value
+        outgoing_msg = username + ": " + outgoing_msg
         mySocket.send(outgoing_msg);
-
-        let newElement = document.createElement("p")
-        newElement.classList.add("this");
-        newElement.innerHTML = outgoing_msg;
-        document.getElementById('chat').appendChild(newElement)
-
+        add_message(outgoing_msg, className= "this");
         document.getElementById("msg").value = "";
     
 })
@@ -43,7 +34,12 @@ function main(){
 }
 
 //global variables
-
+function add_message(value, className = "this") {
+    let newElement = document.createElement("p")
+    newElement.classList.add(className);
+    newElement.innerHTML = value;
+    document.getElementById('chat').appendChild(newElement);
+}
 
     
 
